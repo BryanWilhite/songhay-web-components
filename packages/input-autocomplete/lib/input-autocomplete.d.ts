9ac580d8d061ac17c64bdc11f34fe45e361806cf
Bryan Wilhite <rasx@songhaysystem.com>
2020-05-01 15:04:01 -0700

    :snowflake: :gear: typescript pipeline #1

---
+++ packages/input-autocomplete/lib/input-autocomplete.d.ts
@@ -0,0 +1,1 @@
+declare function inputAutocomplete(): void;
--- packages/input-autocomplete/src/input-autocomplete.ts
+++ packages/input-autocomplete/src/input-autocomplete.ts
@@ -1,6 +1,7 @@
 'use strict';
+
 module.exports = inputAutocomplete;
+
 function inputAutocomplete() {
     // TODO
 }
-//# sourceMappingURL=input-autocomplete.js.map

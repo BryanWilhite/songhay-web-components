194b53358febd40777d30ca6c3d0f913f9cd25c8
Bryan Wilhite <rasx@songhaysystem.com>
2020-12-19 20:37:19 -0800

    📦🚀

--- packages/input-autocomplete/lib/index.js
+++ packages/input-autocomplete/lib/index.js
@@ -1,6 +1,8 @@
 export * from './constants/normalize-css';
+export * from './models/autocomplete-suggestion';
 export * from './models/key';
 export * from './services/autocomplete-suggestions';
+export * from './types/input-modes';
 export * from './input-autocomplete-base';
 export * from './input-autocomplete';
 //# sourceMappingURL=index.js.map
